b71ef0faf89c89e5924246d3f95fae2644077c5e
AnjaliRawke <anjalirawke21@gmail.com>
2023-03-16 09:25:11 +0530

    Added: Workshop2 Repeating Elements in array problem

--- src/com/bridgelabz/RepeatingElementsInArray.java
+++ src/com/bridgelabz/RepeatingElementsInArray.java
@@ -1,14 +1,30 @@
 package com.bridgelabz;
 
-public class RepeatingElementsInArray {
-	public static void main(String[] args) {
-		int arr[] = new int[]{1,2,3,4,2,7,8,8,3,1};
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = i + 1; j < arr.length; j++) {
-				if (arr[i] == arr[j])
-					System.out.println(arr[j]);
-			}
+import java.util.Arrays;
 
+public class RepeatingElementsInArray {
+	public static void countFreq(int arr[],int n)
+	{
+		boolean visited[] = new boolean[n];
+		Arrays.fill(visited,false);
+		for (int i = 0; i < n; i++) {
+			if (visited[i] == true)
+				continue;
+			int count = 1;
+			for (int j = i + 1; j < n; j++) {
+				if (arr[i] == arr[j]){
+					visited[j] = true;
+				count++;
+			}
 		}
+			if(count != 1)
+				System.out.println(arr[i]);
+		}
+	}
+
+	public static void main(String[] args) {
+		int arr[] = new int[]{1,2,3,4,2,7,8,8,3,3,1};
+		int n = arr.length;
+		countFreq(arr,n);
 	}
 }
